b72b8fa93f13baf9b071c117dbad0ae9c71bf2dc
maestrx <github@safar.info>
2015-10-09 22:44:39 +0200

    updatge 1.1
    Added features:
    1.	Output suitable for scripts - choice of delimiter? Then I would choose 'tab'.
    2.	Json output.
    3.	Client side filter. So it can get all data and filter it locally. Would be better than nagrestconf's filtering where you need to know the column number.

--- nagrestconf.py
+++ nagrestconf.py
@@ -3,21 +3,19 @@
 #
 #          FILE: nagrestconf.py
 #   DESCRIPTION: Python binginf for NagRestConf REST interface
-#  REQUIREMENTS: pycurl, json, urllib,StringIO
+#  REQUIREMENTS: pycurl, json, urllib, StringIO, ConfigParser
 #        RETURN: 0 - OK
 #          BUGS: ---
 #         NOTES: Validated using python 2.6
 #        AUTHOR: vit.safar@gemalto.com
 #  ORGANIZATION:
-#       CREATED: 8/10/15
-#      REVISION: 1.0
+#       CREATED: 9/10/15
+#      REVISION: 1.1
 #===============================================================================
 
 
 import sys
 import os
-import re
-import time
 import logging
 import pycurl
 import json
@@ -195,33 +193,84 @@ class nagrestconf:
 
 if __name__ == "__main__":
   import argparse
+  import ConfigParser
+
+  def objSeek(obj,name):
+    for ind,val in enumerate(obj):
+      if name in val:
+          return val[name]
+    return None
+
+  reqshort={
+'cn':['check/nagiosconfig',False],
+'sh':['show/hosts',False], 'ss':['show/services',False], 'sn':['show/contacts',False], 'sm':['show/commands',False] ,
+'mh':['modify/hosts',True], 'ms':['modify/services',True], 'mn':['modify/contacts',True], 'mm':['modify/commands',True] ,
+'dh':['delete/hosts',True], 'ds':['delete/services',True], 'dn':['delete/contacts',True], 'dm':['delete/commands',True] ,
+'ah':['add/hosts',True], 'as':['add/services',True], 'an':['add/contacts',True], 'am':['add/commands',True] ,
+'rn':['restart/nagios',True],
+'nc':['apply/nagiosconfig',True], 'nll':['apply/nagioslastgoodconfig',True] }
 
   data={}
+  filter={}
   post=False
   log=40
+  jout=False
+  sep="\t"
+  conffile=os.path.expanduser("~/.nagrestconf")
+
   try:
     parser = argparse.ArgumentParser(description='NagRESTConf API')
     parser.add_argument('-s','--server', help='Server URI to connect to. It may include HTTP auth data. Ex: https://user:password@restserver/rest')
-    parser.add_argument('-r','--request', help='Request type to run. ex: show/hosts')
+    parser.add_argument('-r','--request', help="Request to run: show/hosts, apply/nagiosconfig, etc. OR shortcuts: \
+cn => check/nagiosconfig , \
+sh => show/hosts , ss => show/services , sn => show/contacts , sm => show/commands , \
+mh => modify/hosts , ms => modify/services , mn => modify/contacts , mm => modify/commands , \
+dh => delete/hosts , ds => delete/services , dn => delete/contacts , dm => delete/commands , \
+ah => add/hosts , as => add/services , an => add/contacts , am => add/commands , \
+rn => restart/nagios , \
+nc => apply/nagiosconfig , nl => apply/nagioslastgoodconfig")
     parser.add_argument('-p','--post', action='store_true', help='Run POST request. Needed for all calls except for check and show.')
+    parser.add_argument('-j','--json', action='store_true', help='Show result in JSON format')
     parser.add_argument('-d','--data', help='Request data pairs (name:value) separated by comma with no spaces. Ex: name:monitoredhost1,svcdesc:ssh,command:check_ssh')
     parser.add_argument('-v', '--verbose', action='count', help='Verbose level. Default verbosity is ERROR. Each -v increases the log level by one step')
+    parser.add_argument('-D','--delimiter', help='Delimiter for the pipe output')
+    parser.add_argument('-f','--filter', help='Filter rule pairs (name:value) separated by comma with no spaces. Ex: name:monitoredhost1,svcdesc:ssh,command:check_ssh')
 
     args = vars(parser.parse_args())
     if args['server']!=None:
       server=args['server']
     else:
-      print 'Server have to be specified!'
-      sys.exit(1)
+      if os.path.isfile(conffile):
+        try:
+          conf = ConfigParser.RawConfigParser()
+          conf.read(conffile)
+          server = conf.get('client', 'server')
+        except:
+          print 'Failed to read server from config file {0}!'.format(conffile)
+          sys.exit(1)
+      else:
+          print 'Server have to be specified!'
+          sys.exit(1)
+
+    if args['post']!=False:
+      post=True
+
+    if args['json']!=False:
+      jout=True
+
+    if args['delimiter']!=None:
+        sep=args['delimiter']
 
     if args['request']!=None:
-      req=args['request']
+        if args['request'] in reqshort:
+            req=reqshort[args['request']][0]
+            post=reqshort[args['request']][1]
+        else:
+            req=args['request']
     else:
       print 'request have to be specified!'
       sys.exit(1)
 
-    if args['post']!=False:
-      post=True
 
     if args['verbose']>0:
       log=40-args['verbose']*10
@@ -237,24 +286,88 @@ if __name__ == "__main__":
             logging.exception('Data argument has to have syntax: <name>:<value>[,<name2>:<value2>,...]  ')
             sys.exit(2)
 
+    if args['filter']!=None:
+      filters=args['filter'].split(',')
+      for filt in filters:
+        try:
+            filt=filt.split(':')
+            filter.update({filt[0]:filt[1]})
+        except:
+            logging.exception('Filter argument has to have syntax: <name>:<value>[,<name2>:<value2>,...]  ')
+            sys.exit(2)
+
     nrc=nagrestconf(server,'folder',log)
     ret=nrc._run(req,data,post)
     if ret[0]:
         if isinstance(ret[1][0], (list, tuple)):
-            print 'Result array:'
-            for elem in ret[1]:
-                print "Element:"
-                for atr in elem:
-                    (aind,aval)=atr.items()[0]
-                    if len(aval)>0:
-                        print '{0}{1:22}{2}'.format(' '*10,aind+':',aval)
-                print
+            if len(filter)>0:
+                prefilter=ret[1]
+                ret[1]=[]
+                for dat in prefilter:
+                    match=True
+                    for filt in filter.items():
+                        f=objSeek(dat,filt[0])
+                        if not (f==None or f==filt[1]):
+                            match=False
+                    if match:
+                        ret[1].append(dat)
+
+            if sys.stdout.isatty():
+                if jout:
+                    print json.dumps(ret[1])
+                else:
+                    print 'Result array:'
+                    for elem in ret[1]:
+                        print "Element:"
+                        for atr in elem:
+                            (aind,aval)=atr.items()[0]
+                            if len(aval)>0:
+                                print '{0}{1:22}{2}'.format(' '*10,aind+':',aval)
+                        print
+            else:
+                if jout:
+                    print json.dumps(ret[1])
+                else:
+                    first=True
+                    for elem in ret[1]:
+                        # show output header
+                        if first:
+                            for atr in elem:
+                                sys.stdout.write('{0}{1}'.format(atr.items()[0][0],sep))
+                            print
+                            first=False
+                        for atr in elem:
+                            sys.stdout.write('{0}{1}'.format(atr.items()[0][1],sep))
+                        print
+
+
+
         elif isinstance(ret[1][0], basestring):
-           print 'Result string: '
-           for elem in ret[1]:
-            print elem
+            if sys.stdout.isatty():
+                if jout:
+                    print json.dumps(ret[1])
+                else:
+                    print 'Result string:'
+                    for elem in ret[1]:
+                       print elem
+            else:
+                if jout:
+                    print json.dumps(ret[1])
+                else:
+                    print "Result_string",
+                    for elem in ret[1]:
+                       print "\n{0}".format(elem)
         else:
-           print 'Result: {0}'.format(ret[1])
+            if sys.stdout.isatty():
+                if jout:
+                    print json.dumps(ret[1])
+                else:
+                    print 'Result: {0}'.format(ret[1])
+            else:
+                if jout:
+                    print json.dumps(ret[1])
+                else:
+                    print "Result\n{0}".format(ret[1])
     else:
         print 'Failed with reason: {0}'.format(ret[1])
 
